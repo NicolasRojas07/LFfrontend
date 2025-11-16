7acd26e066a79f1d7be13a0123637a2f3d05e60b
NicolasRojas07 <149183682+NicolasRojas07@users.noreply.github.com>
2025-11-16 15:04:59 -0500

    Update API base URL to use environment variable

--- src/api.ts
+++ src/api.ts
@@ -1,6 +1,8 @@
-import axios from 'axios';
+import axios from "axios";
+
+const API_URL = import.meta.env.VITE_API_URL;
 
 export const api = axios.create({
-  baseURL: 'http://localhost:5000/api/jwt', // URL backend Flask
-  headers: { 'Content-Type': 'application/json' }
+  baseURL: `${API_URL}/api/jwt`,
+  headers: { "Content-Type": "application/json" }
 });
